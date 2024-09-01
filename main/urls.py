dfa7c82aa1d8f94563d646e036b806b08b546715
DIANAsaad <passiona674@gmail.com>
2024-09-01 15:13:43 +0300

    add these changes

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from main import views
 
 
+
 urlpatterns=[
     path('',views.home),
+    path('home',views.home,name='home'),
+    path('sign-up',views.sign_up, name='sign_up'),
             ]
