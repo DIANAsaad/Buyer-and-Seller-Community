6c8d3fd0fcf1924322064a085d2caa2239926ef2
DIANAsaad <passiona674@gmail.com>
2024-11-03 13:39:23 +0200

    successfuly connected websockets/created layers

--- AuthProject/settings.py
+++ AuthProject/settings.py
@@ -87,7 +87,11 @@ TEMPLATES = [
 # WSGI_APPLICATION = 'AuthProject.wsgi.application'
 ASGI_APPLICATION = 'AuthProject.asgi.application'
 
-
+CHANNEL_LAYERS={
+    'default':{
+        "BACKEND":"channels.layers.InMemoryChannelLayer",
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
